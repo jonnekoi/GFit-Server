4b473b349c8d7826bde06469cca7cc9229099d4e
jonnekoi <jonnekoivisto00@gmail.com>
2025-03-08 22:12:07 +0200

    calculating meal macros when fetching meals

--- src/api/model/mealModel.js
+++ src/api/model/mealModel.js
@@ -1,16 +1,25 @@
 import promisePool from "../../utils/database.js";
 
 const fetchAllMeals = async () => {
-    const query = `
+    const mealQuery = `
         SELECT 
             m.id AS meal_id,
             m.name AS meal_name,
             m.description AS meal_description,
-            m.protein AS total_protein,
-            m.carbs AS total_carbs,
-            m.fat AS total_fat,
-            m.calories AS total_calories,
             m.category AS meal_category,
+            COALESCE(SUM((i.protein_per_100g / 100) * mi.quantity_g), 0) AS total_protein,
+            COALESCE(SUM((i.carbs_per_100g / 100) * mi.quantity_g), 0) AS total_carbs,
+            COALESCE(SUM((i.fat_per_100g / 100) * mi.quantity_g), 0) AS total_fat,
+            COALESCE(SUM((i.calories_per_100g / 100) * mi.quantity_g), 0) AS total_calories
+        FROM meals m
+        LEFT JOIN meal_ingredients mi ON m.id = mi.meal_id
+        LEFT JOIN ingredients i ON mi.ingredient_id = i.id
+        GROUP BY m.id;
+    `;
+
+    const ingredientQuery = `
+        SELECT 
+            mi.meal_id,
             i.id AS ingredient_id,
             i.name AS ingredient_name,
             mi.quantity_g AS ingredient_quantity_g,
@@ -18,45 +27,18 @@ const fetchAllMeals = async () => {
             i.carbs_per_100g,
             i.fat_per_100g,
             i.calories_per_100g
-        FROM meals m
-        LEFT JOIN meal_ingredients mi ON m.id = mi.meal_id
-        LEFT JOIN ingredients i ON mi.ingredient_id = i.id
-        ORDER BY m.id, i.id;
+        FROM meal_ingredients mi
+        JOIN ingredients i ON mi.ingredient_id = i.id;
     `;
 
     try {
-        const [rows] = await promisePool.query(query);
+        const [meals] = await promisePool.query(mealQuery);
+        const [ingredients] = await promisePool.query(ingredientQuery);
 
-        const formattedMeals = {};
-
-        rows.forEach(row => {
-            const { meal_id, meal_name, meal_description, total_protein, total_carbs, total_fat, total_calories, meal_category, ingredient_id, ingredient_name, ingredient_quantity_g, protein_per_100g, carbs_per_100g, fat_per_100g, calories_per_100g } = row;
-
-            if (!formattedMeals[meal_name]) {
-                formattedMeals[meal_name] = {
-                    meal_id,
-                    meal_description,
-                    total_protein,
-                    total_carbs,
-                    total_fat,
-                    total_calories,
-                    meal_category,
-                    ingredients: []
-                };
-            }
-
-            if (ingredient_id) {
-                formattedMeals[meal_name].ingredients.push({
-                    ingredient_id,
-                    ingredient_name,
-                    quantity_g: ingredient_quantity_g,
-                    protein_per_100g,
-                    carbs_per_100g,
-                    fat_per_100g,
-                    calories_per_100g
-                });
-            }
-        });
+        const formattedMeals = meals.map(meal => ({
+            ...meal,
+            ingredients: ingredients.filter(ing => ing.meal_id === meal.meal_id)
+        }));
 
         return formattedMeals;
     } catch (error) {
@@ -64,6 +46,5 @@ const fetchAllMeals = async () => {
         throw error;
     }
 };
-
 
 export default fetchAllMeals;
